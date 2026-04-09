125be7fabedecb4a1525ceb9d16c3460cf9882cf
Hannah Am. <amaritahannah22@gmail.com>
2026-04-09 16:35:24 -0300

    Add files via upload

---
+++ Clase1.py
@@ -0,0 +1,27 @@
+numero=2
+numero2=8
+numero3=15
+resultado=numero+numero2+numero3
+print('numero: ',numero)
+print('numero2: ',numero2)
+print('numero3: ',numero3)
+print('resultado: ',resultado)
+
+#Clase 2
+
+#Calcular los minutos que hay en una semana
+Horas_por_dia=24
+minutos_por_hora=60
+Dias_por_semana=7
+print("En una semana hay de minutos: ",minutos_por_hora * Horas_por_dia * Dias_por_semana)
+
+#Calcular el peso del paquete. Payasos 112g y muñecas 75g
+print(" ")
+print("----------------Ejercicio 2----------------")
+peso_payaso=112
+peso_muneca=75
+cantidad_payasos=int(input("Ingrese la cantidad de payasos: "))
+cantidad_munecas=int(input("Ingrese la cantidad de muñecas: "))
+peso_total= peso_payaso * cantidad_payasos + peso_muneca * cantidad_munecas
+print("El peso total del paquete es: ",peso_total)
+
